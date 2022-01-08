d63ea9afb7b07aa93793d10e4b2ce6f511d51802
Pedro Pacheco <pedro.jm.pacheco@outlook.pt>
2022-01-08 18:55:16 +0000

    fixed a bug

--- Prolog/proj2/ruleset.pl
+++ Prolog/proj2/ruleset.pl
@@ -7,3 +7,14 @@ choosePlayer(PlayerOne, PlayerTwo, FirstToPlay) :-
 
 % ---------------------------------------------------------------
 
+% Working
+changeTurn(CurrentPlayer, NextPlayer) :- 
+    (
+    CurrentPlayer =:= 1 
+    -> NextPlayer = 2
+    ; NextPlayer = 1
+    ).
+    
+% ( condition -> then_clause ; else_clause )
+
+% ---------------------------------------------------------------
