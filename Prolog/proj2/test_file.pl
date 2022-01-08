316a6d63ce5c34f3177c9af4f6f2b24b586682e3
Pedro Pacheco <pedro.jm.pacheco@outlook.pt>
2022-01-08 18:52:41 +0000

    change turn working

--- Prolog/proj2/test_file.pl
+++ Prolog/proj2/test_file.pl
@@ -4,4 +4,12 @@
 choosePlayer(PlayerOne, PlayerTwo, FirstToPlay) :-
     random(PlayerOne, PlayerTwo, FirstToPlay).
 
-% Doing
+% Working
+changeTurn(CurrentPlayer, NextPlayer) :- 
+    (
+    CurrentPlayer =:= 1 
+    -> NextPlayer = 2
+    ; NextPlayer = 1
+    ).
+    
+% ( condition -> then_clause ; else_clause )
