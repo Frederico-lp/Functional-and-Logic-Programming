a0b3ead0ce2a294f1505273312157c7b47d496c5
Pedro Pacheco <pedro.jm.pacheco@outlook.pt>
2022-01-09 00:49:09 +0000

    row and column checking

--- Prolog/proj2/ruleset.pl
+++ Prolog/proj2/ruleset.pl
@@ -1,6 +1,7 @@
 :- use_module(library(random)).
 
-% Working - PlayerOne is 1 and PlayerTwo is 3 (because the upperbound is not included in the interval)
+% Working 
+% PlayerOne is 1 and PlayerTwo is 3 (because the upperbound is not included in the interval)
 
 choosePlayer(PlayerOne, PlayerTwo, FirstToPlay) :-
     random(PlayerOne, PlayerTwo, FirstToPlay).
@@ -8,6 +9,7 @@ choosePlayer(PlayerOne, PlayerTwo, FirstToPlay) :-
 % ---------------------------------------------------------------
 
 % Working
+
 changeTurn(CurrentPlayer, NextPlayer) :- 
     (
     CurrentPlayer =:= 1 
@@ -15,8 +17,6 @@ changeTurn(CurrentPlayer, NextPlayer) :-
     ; NextPlayer = 1
     ).
     
-% ( condition -> then_clause ; else_clause )
-
 % ---------------------------------------------------------------
 
 % Working
@@ -47,4 +47,30 @@ checkVerticalMove(Column, Row, FinalColumn, FinalRow, ReturnBooleanValue) :-
     ;   ReturnBooleanValue = 'False'
     ).
 
+% ---------------------------------------------------------------
+
+% Working
+
+checkInputRow(IsValid):-
+    repeat,
+    read(Number),
+    (
+       memberchk(Number,[0,1,2,3,4,5,6,7])
+    -> IsValid = 'True', !, true
+    ; write('Invalid Row\n'), IsValid = 'False', fail
+    ).
+
+% ---------------------------------------------------------------
+
+% Working
+
+checkInputColumn(IsValid):-
+    repeat,
+    read(Number),
+    (
+       memberchk(Number,[0,1,2,3,4,5,6,7,8,9,10,11])
+    -> IsValid = 'True', !, true
+    ; write('Invalid Column\n'), IsValid = 'False', fail
+    ).
+    
 % ---------------------------------------------------------------
